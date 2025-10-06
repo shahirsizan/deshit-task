431921753a930777e9f0fede76a82dfbc0070c3d
shahirsizan <shahir.sizan18@gmail.com>
2025-10-06 14:21:28 +0600

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+	return (
+		<div className="h-screen ">
+			Navbar <br />
+			Hero
+			<br /> My services
+			<br /> Contact me
+			<br /> My skills
+			<br /> My projects
+			<br /> client reviews
+			<br /> footer
+		</div>
+	);
+}
+
+export default App;
