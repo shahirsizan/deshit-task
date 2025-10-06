f4c28f6634d60f6bf1599511f89d9d0bf0fdedca
shahirsizan <shahir.sizan18@gmail.com>
2025-10-06 18:10:28 +0600

    Navbar made sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,23 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<div className="border-b-2 flex items-center justify-between py-3 px-[5vw] md:px-[8vw] lg:px-[10vw]">
+		<div
+			style={{
+				background: `linear-gradient(
+					to right,
+					#131339 0%,
+					#232369 25%,
+					#35359f 50%,
+					#232369 75%,
+					#060606 100%
+				)`,
+			}}
+			className="text-white sticky top-0 border-b-2 flex items-center justify-between py-3 px-[5vw] md:px-[8vw] lg:px-[10vw]"
+		>
 			<div>
 				<span className="text-[#B94A5B]">ARAFAT</span> HOSSAIN SOBUJ
 			</div>
+
 			<div className="flex items-center gap-2">
 				<a href="#home">Home</a>
 				<a href="#about">About</a>
