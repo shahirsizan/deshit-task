5268b7cd4f7eccc2a889f892a1d471c829f2b09d
shahirsizan <shahir.sizan18@gmail.com>
2025-10-08 23:23:40 +0600

    App.jsx done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import MyServices from "./components/MyServices";
+import ContactMe from "./components/ContactMe";
+import MySkills from "./components/MySkills";
+import MyProjects from "./components/MyProjects";
+import ClientReviews from "./components/ClientReviews";
+import Footer from "./components/Footer";
+
 function App() {
 	return (
-		<div className="h-screen ">
-			Navbar <br />
-			Hero
-			<br /> My services
-			<br /> Contact me
-			<br /> My skills
-			<br /> My projects
-			<br /> client reviews
-			<br /> footer
+		<div className="relative min-h-screen">
+			<Navbar />
+			<Hero />
+			<MyServices />
+			<ContactMe />
+			<MySkills />
+			<MyProjects />
+			<ClientReviews />
+			<Footer />
 		</div>
 	);
 }
