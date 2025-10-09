67be518e58057691e80d1ad70a7c336b482f9349
shahirsizan <shahir.sizan18@gmail.com>
2025-10-09 22:48:39 +0600

    Animation balls config added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export const theme = {
+	extend: {
+		animation: {
+			// from `index.css`
+			float1: "float1 6s ease-in-out infinite",
+			float2: "float2 8s ease-in-out infinite",
+			float3: "float3 5s ease-in-out infinite",
+		},
+		// keyframes: {
+		// 	not needed because already prescribed in `index.css`
+		// },
+	},
+};
+export const plugins = [];
